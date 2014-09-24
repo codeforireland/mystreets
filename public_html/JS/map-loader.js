b0f01884b22a776181aefafd5f31080aa834372a
eoinzy <eoinconroy@gmail.com>
2014-09-24 23:59:20 +0100

    Updated home page to add logo. Edited javascript to add in some markers.

--- public_html/JS/map-loader.js
+++ public_html/JS/map-loader.js
@@ -5,8 +5,21 @@ $(document).ready(function()
     map = new GMaps({
         el: '#map_canvas',
         lat: 53.346046,
-        lng: -6.260094
-    });
+        lng: -6.260094,
+		zoom: 12,
+		zoomControl : true,
+		zoomControlOpt: {
+			style : 'SMALL',
+			position: 'TOP_LEFT'
+		},
+		panControl : false,
+		click: function(e){
+			addCustomMarkerToMap();
+		},
+		dragend: function(e){
+		  alert('Drag Event');
+		}
+	  });
 
     map.setContextMenu({
         control: 'map',
@@ -26,6 +39,27 @@ $(document).ready(function()
                 }
             }]
     });
+	/*
+	GMaps.geolocate({
+    success: function(position){
+      map.setCenter(position.coords.latitude, position.coords.longitude);
+
+      map.addMarker({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+        title: 'You are here.',
+        infoWindow: {
+          content: '<p>You are here!</p>'
+        }
+      });
+    },
+    error: function(error){
+      alert('Geolocation failed: '+error.message);
+    },
+    not_supported: function(){
+      alert("Your browser does not support geolocation");
+    }
+  });*/
 
     map.setContextMenu({
         control: 'marker',
@@ -39,3 +73,7 @@ $(document).ready(function()
             }]
     });
 });
+
+function addCustomMarkerToMap() {
+    alert('Click event');
+}
